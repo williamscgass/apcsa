57f031df3ed87a2eace6788904a6db8d9b75aabe
williamscgass <william.scgass@gmail.com>
2023-10-30 16:50:05 -0500

    for loop demos

--- lec-notes/10-30-2023/Main.java
+++ lec-notes/10-30-2023/Main.java
@@ -1,7 +1,62 @@
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello, world!");
 
+        int[] numbers = { 1, 4, 6, 7, 2, 5 };
+
+        // ITERATING WITH WHILE LOOP
+        /* int i = 0;
+        while (i < numbers.length) {
+            System.out.println(numbers[i]);
+            i++; // i += 1, i = i + 1 are equivalent 
+        }
+        System.out.println("");
+
+        i = 0;
+        while (i < numbers.length) {
+            numbers[i] += 1;
+            if (numbers[i] == 21) {
+                break;
+            } 
+
+            i++; // i += 1, i = i + 1 are equivalent 
+        } */
+
+        int i = 0;
+        while (i < numbers.length) {
+            System.out.println(numbers[i]);
+            // some more code here...
+
+            i++; // i += 1, i = i + 1 are equivalent 
+        }
+
+        System.out.println("");
+
+       /*  for (i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 3) {
+                continue;
+            }
+
+            numbers[i] *= i;
+            System.out.println(numbers[i]);
+        } */
+
+        for (int number : numbers) {
+            number = number * 2;
+            if (number > 10) {
+                continue;
+            }
+
+            System.out.println(number);
+        }
+
+        System.out.println("");
+
+        i = 0;
+        while (i < numbers.length) {
+            System.out.println(numbers[i]);
+            // some more code here...
+
+            i++; // i += 1, i = i + 1 are equivalent 
+        }
     }
 }
